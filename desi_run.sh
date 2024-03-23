64c995eefdbdd77db786e1493f3280519d12f39b
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-03-22 22:19:18 -0700

    Automatically fix credentials format

--- desi_run.sh
+++ desi_run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Add DESI Python libraries to PATH and PYTHONPATH
+# so they can be easily imported
+
 pushd $DESI_HUB
 for package in desiutil specter desitarget desispec desisim desimodel redrock desisurvey surveysim; do
   export PATH=$DESI_HUB/$package/bin:$PATH
